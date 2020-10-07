604cb370f8a6eeafac43dc3d233e6d152917b527
iac26 <iacopo.sprenger@epfl.ch>
2020-10-07 23:02:33 +0200

    imgs

--- python/csv_reader.m
+++ python/csv_reader.m
@@ -4,19 +4,31 @@ hold on
 plot(T.(15), T.(10))
 plot(T.(15), T.(11))
 legend("actual position", "demand position")
+xlim([195000 203300]);
+xlabel("time [ms]");
+ylabel("position [inc]");
 hold off
+saveas(gcf,'1.png')
 figure;
 hold on
 plot(T.(15), T.(12))
 plot(T.(15), T.(13))
 legend("actual current", "demand current")
+xlim([195000 203300]);
+xlabel("time [ms]");
+ylabel("current [mA]");
 hold off
+saveas(gcf,'2.png')
 
 figure;
 hold on
 plot(T.(15), T.(14))
 legend("actual velocity")
+xlim([195000 203300]);
+xlabel("time [ms]");
+ylabel("velocity [rpm]");
 hold off
+saveas(gcf,'3.png')
 
 figure;
 hold on
