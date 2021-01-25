5a6f3d58999accf607a4aebc9243c25dd2099d40
iac26 <iacopo.sprenger@epfl.ch>
2021-01-25 11:46:01 +0100

    started working on epos4 driver

---
+++ Version_2/Application/Inc/epos4.h
@@ -0,0 +1,56 @@
+/*  Title       : Maxon EPOS4 driver
+ *  Filename    : epos4.h
+ *  Author      : iacopo sprenger
+ *  Date        : 25.01.2021
+ *  Version     : 0.1
+ *  Description : Maxon epos 4 board driver
+ */
+
+#ifndef EPOS4_H
+#define EPOS4_H
+
+/**********************
+ *  INCLUDES
+ **********************/
+
+#include <stdint.h>
+
+/**********************
+ *  CONSTANTS
+ **********************/
+
+
+
+/**********************
+ *  MACROS
+ **********************/
+
+
+/**********************
+ *  TYPEDEFS
+ **********************/
+
+
+
+
+/**********************
+ *  VARIABLES
+ **********************/
+
+
+/**********************
+ *  PROTOTYPES
+ **********************/
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+
+#ifdef __cplusplus
+} // extern "C"
+#endif /* __cplusplus */
+
+#endif /* EPOS4_H */
+
+/* END */
